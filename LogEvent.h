0f4ea97d4d2668062c712fa8e073704bf70fef36
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-07-26 18:22:49 +0200

    Log event with time

--- LogEvent.h
+++ LogEvent.h
@@ -1,21 +1,36 @@
+#include <ctime>
+
 #include "Displayable.h"
 
 struct LogEvent
 {
-    LogEvent(const std::string& _callerName, int _callerLine, Displayable&& _event)
-          : callerName(_callerName)
+    LogEvent(const std::time_t& _time, const std::string& _callerName, int _callerLine, Displayable&& _event)
+          : time(_time)
+          , callerName(_callerName)
           , callerLine(_callerLine)
           , event(std::move(_event))
     {
     }
 
+    std::time_t time;
     std::string callerName;
     int callerLine;
     Displayable event;
 };
 
+std::string dateTime(const time_t& now)
+{
+    struct tm* tstruct = localtime(&now);
+
+    char buf[30];
+    strftime(buf, sizeof(buf), "%Y-%m-%d %X", tstruct);
+
+    return buf;
+}
+
 void display(const LogEvent& log)
 {
+    std::cout << "[" << dateTime(log.time) << "]";
     std::cout << "[" << log.callerName << ":" << log.callerLine << "]";
     std::cout << " ";
     display(log.event);
--- Logger.h
+++ Logger.h
@@ -1,15 +1,15 @@
 #include "LogEvent.h"
 
-#define LOG(logger, event) (logger.record(__FUNCTION__, __LINE__, event))
+#define LOG(logger, event) (logger.record(std::time(0), __FUNCTION__, __LINE__, event))
 
 class Logger
 {
 public:
     Logger() = default;
 
-    void record(const std::string& callerName, int callerLine, Displayable&& event)
+    void record(const std::time_t& time, const std::string& callerName, int callerLine, Displayable&& event)
     {
-        m_events.emplace_back(callerName, callerLine, std::move(event));
+        m_events.emplace_back(time, callerName, callerLine, std::move(event));
     }
 
     void displayEvents() const
